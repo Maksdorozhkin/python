5e45666eb082ecee172d4c0e4d6b88743543bd66
md <maksdorozhkin@gmail.com>
2022-03-29 22:41:42 +0300

    name_cases.py

---
+++ name_cases.py
@@ -0,0 +1,30 @@
+
+f_name = "   maks  "
+l_name = "   dorozhkin  "
+ful_name = f"\n\t{f_name.strip()} \n\t{l_name.strip()}" 
+message = f"\tHello, {ful_name.title()}"
+print(message)
+
+print(5 + 3)
+print(2 * 4)
+print(16 / 2)
+print(10 - 2)
+
+number = 888
+message = f"\t\t\t{number}"
+print(message)
+
+#MD
+n_1 = 5+3 
+n_2 = 2*4
+n_3 = 16/2
+n_4 = 10-2
+message= f"\n\t{n_1} \n\t{n_2} \n\t{n_3} \n\t{n_4}" #переменная выводит значения четырех переменных с новой стороки и с нового абзаца 
+print(message)
+
+
+
+
+
+
+
