bd403b2c7209b2293c265cda53f1b050fe11e510
md <maksdorozhkin@gmail.com>
2022-03-29 22:38:56 +0300

    name_cases.py

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+message = 'Albert Einshteine once said,"A person who never made a mistake never tried anything new."'
+print(message)
+
+f_person = "Albert Einshteine"
+message = 'once said,"A person who never made a mistake never tried anything new"'
+print(f_person, message)
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+message = "Hello Python world!"
+print(message) 
+
+message = "Hello Python crash Course world!"
+print(mesage)
+
