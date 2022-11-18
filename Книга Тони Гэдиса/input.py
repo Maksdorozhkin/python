52a204f5d37cfe0d3679145c96b2268762322043
md <maksdorozhkin@gmail.com>
2022-11-18 14:12:58 +0300

    Начало моего изучения python

---
+++ Книга Тони Гэдиса/input.py
@@ -0,0 +1,9 @@
+# Получить имя, возвраст и доход пользователя
+name = input('Как Вас зовут? ')
+age = int(input('How old are you? '))
+income = float(input('Какой у вас доход? '))
+# вывод данных на экран
+print('Вот данные, которые Вы ввели:')
+print('Имя', name)
+print('Возвраст', age)
+print('Доход', income)
