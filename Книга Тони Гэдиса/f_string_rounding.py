13ef1e44367e897e89ac9661795017a9a53f7642
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-11-18 14:58:27 +0300

    изучаем f строки

---
+++ Книга Тони Гэдиса/f_string_rounding.py
@@ -0,0 +1,5 @@
+# эта программа показывает округление числа в f строках с плавающей точкой
+
+amount_due = 5000.0
+monthly_paiment = amount_due / 12
+print(f"Ежемесяный платеж {monthly_paiment:.2f}.")
