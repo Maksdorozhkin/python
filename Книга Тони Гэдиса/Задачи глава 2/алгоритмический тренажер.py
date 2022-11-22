69dac404b4cc18f38ce27026ea36ddf6f8a00090
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-11-23 00:11:39 +0300

    Алгоритмические задачи глава 2

---
+++ Книга Тони Гэдиса/Задачи глава 2/алгоритмический тренажер.py
@@ -0,0 +1,27 @@
+# height = input('Введите свой рост ')
+# print(height)
+# color = input('Введите свой любимый цвет ')
+# print(color)
+
+a = ('b') * 4
+print(a)
+
+w = 5
+x = 4
+y = 8
+z = 2
+rezult = x + y
+rezult = z * 2
+rezult = y / x
+rezult = y - z
+rezult = w // z
+print(rezult)
+
+total = 10 + 14
+print(total)
+down_payment = rezult
+due = down_payment - total
+print(due)
+subtotal = 8
+total = subtotal * 0.15
+
