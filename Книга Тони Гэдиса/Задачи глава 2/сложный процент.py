795803a606fef919db25a88c5a7d56876e57e1cf
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-11-27 00:45:38 +0300

    задачи глава 2 задача 15 нудная хрень с черепашьей графикой почти решил

--- Книга Тони Гэдиса/Задачи глава 2/сложный процент.py
+++ Книга Тони Гэдиса/Задачи глава 2/сложный процент.py
@@ -3,6 +3,6 @@ a_i_r = float(input('Введите годовую процентную став
 accrual_of_interest = float(input('Введи частоту начисления процентов в месяцах: '))
 deposite_term = float(input('Введите срок депозита в годах: '))
 
-result = deposite * (1 + (a_i_r / 100) / accrual_of_interest) ** (accrual_of_interest * deposite_term)
+result = (deposite * (1 + (a_i_r / 100) / accrual_of_interest) ** (accrual_of_interest * deposite_term))
 
 print(f'{result:.2f}')
