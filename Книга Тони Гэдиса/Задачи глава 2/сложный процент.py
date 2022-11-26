4138ca3e771d083ffdd13f5c2f8caeec1283ec50
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-11-26 15:29:40 +0300

    задачи глава 2 сложный процент

---
+++ Книга Тони Гэдиса/Задачи глава 2/сложный процент.py
@@ -0,0 +1,8 @@
+deposite = float(input('Введите депозит:'))
+a_i_r = float(input('Введите годовую процентную ставку: '))
+accrual_of_interest = float(input('Введи частоту начисления процентов в месяцах: '))
+deposite_term = float(input('Введите срок депозита в годах: '))
+
+result = deposite * (1 + (a_i_r / 100) / accrual_of_interest) ** (accrual_of_interest * deposite_term)
+
+print(f'{result:.2f}')
