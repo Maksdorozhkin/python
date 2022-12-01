e9d6caa066580b2820e389453d9a6008356fbb31
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-12-01 17:56:22 +0300

    глава 3 Арифметический треножер решение задач № 5 понравилась пришлось задуматься

---
+++ Книга Тони Гэдиса/Глава 3/Задачи глава 3 /арифметический треножер.py
@@ -0,0 +1,41 @@
+# x = int(input(' Введите число'))
+# if x > 100:
+#     y = 20
+#     z = 40
+#     print(y, z)
+#
+# a = int(input('Введите число для а '))
+# if a < 10:
+#     b = 0
+#     c = 1
+
+# a = int(input('Введите число для а '))
+# if a < 10:
+#     b = 0
+# else:
+#     b = 99
+# print(b)
+
+# 5
+# крутая задача
+# amount1 = int(input('Введите значение для amount1 '))
+# amount2 = int(input('Введите знпчение для amount2 '))
+# if amount1 > 10 and amount2 < 100:
+#     if amount1 > amount2:
+#         print(amount1)
+#     else:
+#         print(amount2)
+
+#
+# speed = int(input('Введите скорость '))
+# if speed > 26 and speed < 56:
+#     print('Скорость норм')
+# else:
+#     print('Скорость аварийная! ')
+
+
+points = int(input('Введи значение для переменной points '))
+if points > 9 and points < 51:
+    print('Допустимые точки!')
+else:
+    print('Недопустимые точки! ')
