81d3d6576f9fd0e5580dacc99412f3ec8b50ad1e
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2022-12-02 19:44:18 +0300

    глава 3 дорешал орифмитический тренажер

--- Книга Тони Гэдиса/Глава 3/Задачи глава 3 /арифметический треножер.py
+++ Книга Тони Гэдиса/Глава 3/Задачи глава 3 /арифметический треножер.py
@@ -34,8 +34,51 @@
 #     print('Скорость аварийная! ')
 
 
-points = int(input('Введи значение для переменной points '))
-if points > 9 and points < 51:
-    print('Допустимые точки!')
-else:
-    print('Недопустимые точки! ')
+# points = int(input('Введи значение для переменной points '))
+# if points > 9 and points < 51:
+#     print('Допустимые точки!')
+# else:
+#     print('Недопустимые точки! ')
+
+import turtle
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+turtle.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+turtle.setheading(30.0)
+
+if turtle.heading() >= 0 and turtle.heading() <= 45:
+    turtle.penup()
+if turtle.pencolor('red') and turtle.pencolor('blue'):
+    turtle.pensize(5)
+
+# TARGET_LEFT_X = 100
+# TARGET_LEFT_Y = 100
+# TARGET_RIGHT_X = 200
+# TARGET_RIGHT_Y = 200
+# turtle.speed(2)
+# turtle.penup()
+# turtle.goto(TARGET_LEFT_X, TARGET_LEFT_Y)
+# turtle.setheading(0)
+# turtle.pendown()
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(150)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(150)
+LEFT_UP_ANGLE_X = 100
+LEFT_UP_ANGLE_Y = 100
+RIGHT_BOTTOM_ANGLE_X = 200
+RIGHT_BOTTOM_ANGLE_Y = 200
+WITH = 100
+if (turtle.xcor() >= LEFT_UP_ANGLE_X
+        and turtle.xcor() <= (LEFT_UP_ANGLE_X + WITH)
+        and turtle.ycor() >= RIGHT_BOTTOM_ANGLE_X
+        and turtle.ycor() <= (RIGHT_BOTTOM_ANGLE_Y + WITH)):
+    turtle.hideturtle()
+# смысл этой задачи понятен я не стал заморачиваться просто написал как это работает приблизительно
+# если будет конкретная цель расчитаю точно
