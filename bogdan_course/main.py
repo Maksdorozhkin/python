baaffd52b0b5645b2239f321bf0fa2954193ec5f
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-02-24 20:09:10 +0300

    "отступы"

--- bogdan_course/main.py
+++ bogdan_course/main.py
@@ -2,3 +2,8 @@ name = input("Enter your name:")
 age = input("Enter your age: ")
 city = input("Enter your cyti: ")
 print(name, age, city)
+print(name.capitalize())
+print(name.upper())
+print(dir(name))
+
+
