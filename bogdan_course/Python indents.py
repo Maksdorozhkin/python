5dd0d11c6b837cb46d3de5cd3e6ea3b429b5ae8d
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-06 15:33:31 +0300

    Начало курса

--- bogdan_course/Python indents.py
+++
@@ -1,5 +0,0 @@
-def my_name(name):
-    print(name)
-
-    my_name('Maks')
-my_name('Maks')
--- упражнение 6_1.py
+++ упражнение 6_1.py
@@ -3,6 +3,22 @@ my_friend = {
     'las_name': 'timofeev',
     'age': 49,
     'city': 'dedovsk'
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 print(f"{my_friend['first_name']}\n{my_friend['las_name']}\n{my_friend['age']}"
       f"\n{my_friend['city']}")
