09e1b005bd63943ab547c28c74214e2d3bbe512b
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-06 15:27:15 +0300

    Курс по Python введение

--- bogdan_course/main.py
+++ bogdan_course/main.py
@@ -1,9 +1,5 @@
-name = input("Enter your name:")
-age = input("Enter your age: ")
-city = input("Enter your cyti: ")
-print(name, age, city)
-print(name.capitalize())
-print(name.upper())
-print(dir(name))
+def my_name(name):
+    print(name)
 
 
+my_name("maks")
