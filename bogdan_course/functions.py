9419b8420f387699563d03651c4d668814864ee5
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-14 13:24:38 +0300

    Разбор функций повтор

--- bogdan_course/functions.py
+++ bogdan_course/functions.py
@@ -1,7 +1,19 @@
-def hello(name):
-    print('Hello there,', name)
-    print('Hi there,', name)
+# def hi(name):
+#     print('Hello there!', name)
+#     print('Hi there!', name)
+#
+#
+# hi('Maks') # вызов функции
+# hi('Galya')
+
+def sum_numbs(a, b):
+    sum = a + b
+    return (sum)
+    print('Line is not executed')
 
 
-hello('Maks')
-hello('Alice')
+first_sum = sum_numbs(10, 5)
+print(first_sum)
+# print(sum_numbs(50.5, 20))
+#
+# print(sum_numbs(sum_numbs(50.5, 20),30))
