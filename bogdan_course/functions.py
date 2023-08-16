3226715102b381d4fb3f64f8f9b999d61bf2af2a
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-16 19:47:10 +0300

    Динамическая типизация

--- bogdan_course/functions.py
+++ bogdan_course/functions.py
@@ -15,5 +15,23 @@ def sum_numbs(a, b):
 first_sum = sum_numbs(10, 5)
 print(first_sum)
 print(sum_numbs(50.5, 20))
+
+
 #
 # print(sum_numbs(sum_numbs(50.5, 20),30))
+
+# Динамическая типизация пример
+
+def print_name(name):
+    print(name)
+
+
+print_name('Maks')
+
+# далее  меняем print_name на число
+print_name = 10
+
+# и снова вызываем функцию
+print_name('Maks')
+
+# такой код выдаст ошибку т.к. print_name теперь числ
