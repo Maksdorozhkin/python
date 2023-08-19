e4ae3b45d29eed2b218816835edf41fae9eee366
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-19 16:07:24 +0300

    числа с плавающей точкой

---
+++ bogdan_course/float.py
@@ -0,0 +1,12 @@
+average_price = 20.34
+print(average_price)
+print(type(average_price))
+
+str_temperature = 79.6987765
+temperature = float(str_temperature)
+# temperature = int(str_temperature)
+print(temperature)
+print(type(temperature))
+
+# округление чисел с десятичной точкой
+print(round(str_temperature))
