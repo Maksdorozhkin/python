bb5155a36fddef5c99a1a2a91aeb1a297c02fc86
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-19 23:07:06 +0300

    Логические типы

--- bogdan_course/buildInFunctions.py
+++ bogdan_course/buildInFunctions.py
@@ -1,2 +1,22 @@
-print(10, 'Maksim', True)
-print(dir(__builtins__))
+# print(10, 'Maksim', True)
+# print(dir(__builtins__))
+
+print(-5 > 0)
+print('Long string' > 'Long')  # строки сравниваются посимвольно
+print([1, 2, 3] == [1, 2, 3])
+
+# конвертация в логический тип
+
+db_is_avaible = False
+print(db_is_avaible)
+print(type(db_is_avaible))
+
+db_is_avaible = True
+print(db_is_avaible)
+print(type(db_is_avaible))
+
+print(bool(10))
+print(bool('abc'))
+print(bool([]))
+print(bool([1, 3]))
+print(bool(None))
