aa0ec598c2c2d17eb08b2a4ecb0decd9c8bad0f7
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2023-08-22 15:02:04 +0300

    Списки

--- bogdan_course/list.py
+++ bogdan_course/list.py
@@ -3,8 +3,22 @@ print(maks_input)
 del maks_input[-1]
 print(len(maks_input))
 
-friends = [
+users = [
     {
-        ''
+        'user_id': 100,
+        'user_name': 'Maks'
+    },
+    {
+        'user_id': 101,
+        'user_name': 'Andrew'
     }
 ]
+print(users[-1]['user_name'])
+
+# использование переменных в списках
+
+my_fruit = 'apple'
+other_fruit = 'banana'
+new_fruit = 'lime'
+all_fruite = [my_fruit, other_fruit, new_fruit]
+print(all_fruite)
