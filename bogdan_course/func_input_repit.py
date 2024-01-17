ebaf27e8b11f3773db81c5d884c9eacc793cecaf
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2024-01-17 14:16:32 +0300

    repit bog_course 15-21 create new branch

---
+++ bogdan_course/func_input_repit.py
@@ -0,0 +1,7 @@
+your_info = input("Enter your name, your age and your city: ")
+print(your_info)
+
+
+print(your_info.capitalize())
+print(your_info.upper())
+print(dir(your_info)) # выводит список всех атрибутов объекта your_info
---
+++ bogdan_course/func_print_dir.py
@@ -0,0 +1,5 @@
+print(10, "Maks", True)
+print(print)
+print(input)
+print(dir())
+print(dir(__builtins__)) # показывает атрибуты встроиных функций
