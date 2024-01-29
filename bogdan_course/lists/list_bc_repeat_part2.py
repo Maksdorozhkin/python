b8a9176b58843afc7bad63b720cc07a9256a7b4c
MD <maksdorozhkin@gmail.com>
2024-01-29 19:31:31 +0300

    арифметические операции в списках, нарезка, объединение

--- bogdan_course/lists/list_bc_repeat_part2.py
+++ bogdan_course/lists/list_bc_repeat_part2.py
@@ -2,7 +2,6 @@ inputs = [True, 'Hi!', '😎', 10.5]
 removed_element = inputs.pop(-2)
 print(removed_element)
 
-
 post_ids = [1, 2, 3, 4, 7, 6]
 post_ids.sort()
 print(post_ids)
@@ -16,7 +15,25 @@ greeting = 'Hi from madhouse'
 greeting_letters = list(greeting)
 print(greeting_letters)
 
-
 my_dict = {'a': 10, 'b': 20}
 my_dict_key = list(my_dict)
 print(my_dict_key)
+
+# арифметические операции в списках
+
+ratings = [2.1, 2.4, 3.1, 4.5]
+print(min(ratings))
+print(max(ratings))
+print(sum(ratings))
+print(sum(ratings) / len(ratings))
+
+# объединение списков
+
+my_ratings = [2, 3, 5]
+all_ratings = ratings + my_ratings
+print(all_ratings)
+
+# нарезка списков
+
+first_two_ratings = ratings[1:-1]
+print(first_two_ratings)
