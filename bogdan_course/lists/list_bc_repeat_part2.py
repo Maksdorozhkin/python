606c23d5e9155d16ee67c864007d70accd37a8c5
MD <maksdorozhkin@gmail.com>
2024-01-29 19:13:10 +0300

    практикую списки, методы pop sort

---
+++ bogdan_course/lists/list_bc_repeat_part2.py
@@ -0,0 +1,14 @@
+inputs = [True, 'Hi!', '😎', 10.5]
+removed_element = inputs.pop(-2)
+print(removed_element)
+
+
+post_ids = [1, 2, 3, 4, 7, 6]
+post_ids.sort()
+print(post_ids)
+
+post_ids.sort(reverse=True)
+print(post_ids)
+
+# конвертация в список
+
