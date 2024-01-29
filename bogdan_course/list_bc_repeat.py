00eaaa2b335dc1b5252b7ec102a999c0774ded53
MD <maksdorozhkin@gmail.com>
2024-01-29 18:44:07 +0300

    magic method and lists learn

---
+++ bogdan_course/list_bc_repeat.py
@@ -0,0 +1,31 @@
+my_bike = ['ducati', 'kawasaki']
+my_first_last_bike = ['kawasaki', 'ducati']
+print(my_bike == my_first_last_bike)
+
+print(len(my_bike))
+
+print(my_bike[0])
+print(my_bike[0].capitalize())
+del my_bike[-1]
+print(my_bike)
+
+bikes = [
+    {
+        'first_bike': 'kawasaki',
+        'hors_power': 72
+    },
+    {
+        'last_bike': 'ducati',
+        'hors_power': 112
+    }
+]
+print(len(bikes))
+print(bikes[-1]['hors_power'])
+print(bikes.__len__())
+
+my_fruit = 'apple'
+other_fruit = 'banana'
+new_fruit = 'pineapple'
+all_fruit=['my_fruit', 'other_fruit', 'new_fruit']
+print(all_fruit)
+
--- bogdan_course/magic_method.py
+++ bogdan_course/magic_method.py
@@ -5,6 +5,14 @@ str_val = "abc"
 # print(int_num * str_val)
 
 
-print(float_num.__mul__(str_val))
-# # notimplemented
-print(str_val.__rmul__(float_num))
+# print(float_num.__mul__(str_val))
+# # # notimplemented
+# print(str_val.__rmul__(float_num))
+
+print(dir(bool))
+print(bool.__abs__)
+print(bool.__doc__)
+
+
+my_list = []
+print(help(my_list.__eq__))
