b21bed7abd6b4c79a4c496fde6b39da6875158cc
MD <maksdorozhkin@gmail.com>
2024-02-02 10:36:44 +0300

    Словари повторение материала

---
+++ bogdan_course/dict/dict_bc_rep.py
@@ -0,0 +1,16 @@
+# структура и синтаксис
+my_motorbike = {
+    'brand': 'Ducati',
+    'price': 25000,
+    'engine_vol': 1.2
+}
+
+# Чтобы сравнить два словаря можно использовать оператор ==
+
+# изменение и получения значений в словарях
+print(my_motorbike['brand'])
+my_motorbike['brand'] = 'KTM'
+print(my_motorbike)
+
+print(dir(my_motorbike))
+
