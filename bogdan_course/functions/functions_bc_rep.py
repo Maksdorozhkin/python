f2ff8bea1c95682eea683613c1468db1a7987ea5
MD <maksdorozhkin@gmail.com>
2024-02-10 19:07:09 +0300

    Передача изменяемых объектов в функцию

--- bogdan_course/functions/functions_bc_rep.py
+++ bogdan_course/functions/functions_bc_rep.py
@@ -21,7 +21,6 @@ b = 10
 
 sum(a, b)
 
-
 a = 9
 b = 10
 
@@ -29,17 +28,22 @@ sum(a, b)
 
 print(type(sum(a, b)))
 
+
 def my_fn(a1, b1):
     a1 = a1 + 1
     c1 = a1 + b1
-    return c1 # функция прекращает свою работу
+    return c1  # функция прекращает свою работу
+
 
 res = my_fn(10, 20)
 print(res)
 
+
 # самая короткая функция pass делает функцию не пустой чтоб python не ругался
 def my_pass_function():
     pass
+
+
 print(my_pass_function())
 
 
@@ -50,9 +54,28 @@ def my_fn2(a, b):
     c2 = a + b
     return c2
 
+
 num_one = 10
 num_two = 5
 
 res2 = my_fn2(num_one, num_two)
 print(res2)
 print(num_one)
+
+
+# передача изменяемых объектов
+
+def increase_person_age(person):
+    print(id(person))
+    person['age'] += 1
+    return person
+
+
+person_one = {
+    'name': 'Maks',
+    'age': 42
+}
+
+increase_person_age(person_one)
+print(person_one['age'])
+print(id(person_one))
