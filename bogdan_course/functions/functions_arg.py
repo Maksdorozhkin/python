22961a79b78cee49f46211833c091e407e922aad
MD <maksdorozhkin@gmail.com>
2024-02-11 10:14:43 +0300

    объединение аргументов с ключевыми словами в словарь

--- bogdan_course/functions/functions_arg.py
+++ bogdan_course/functions/functions_arg.py
@@ -7,3 +7,19 @@ def get_post_info(name, post_qty):
 
 info = get_post_info(name='Maks', post_qty=25)
 print(info)
+
+
+# объединение аргументов с ключевыми словами в словарь
+
+def post_info(**person):
+    print(person)
+    # print(type(person))
+    # info_ = (f"{person['name']} wrote "
+    #          f"{person['posts_qty']} posts")
+
+    info_ = f"{person['name']} wrote, {person['posts_qty']} posts"
+    return info_
+
+
+info = post_info(name='Maksim', posts_qty=100)
+print(info)
