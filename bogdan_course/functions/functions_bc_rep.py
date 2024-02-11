c0d6663ec2cc284abadcf348057e532e8b460ff9
MD <maksdorozhkin@gmail.com>
2024-02-11 08:16:49 +0300

    создание копии объекта внутри функции

--- bogdan_course/functions/functions_bc_rep.py
+++ bogdan_course/functions/functions_bc_rep.py
@@ -79,3 +79,22 @@ person_one = {
 increase_person_age(person_one)
 print(person_one['age'])
 print(id(person_one))
+
+
+# создание копии объекта внутри функции
+
+def increase_person_age_copy(person):
+    person_copy = person.copy()
+    person_copy['age'] += 1
+    return person_copy
+
+
+person_one = {
+    'name': 'Maks',
+    'age': 42
+}
+
+new_person = increase_person_age_copy(person_one)
+
+print(new_person['age'])
+print(person_one['age'])
