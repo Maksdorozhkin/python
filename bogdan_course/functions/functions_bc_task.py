467dd196070e2b06fb2a0205d02a2325c8297b39
MD <maksdorozhkin@gmail.com>
2024-02-11 08:52:11 +0300

    Задача функция объединяет два списка, конвертирует их в словарь, короткий вариант

--- bogdan_course/functions/functions_bc_task.py
+++ bogdan_course/functions/functions_bc_task.py
@@ -9,8 +9,16 @@ list2 = [100, 200, 300, 400, 500]
 
 print(merge_lists_to_dict(list1, list2))
 
-
 res_two = merge_lists_to_dict(['abc'], [{}, True, 100])
 print(res_two)
 
+
 # функция объединяет два списка, конвертирует их в словарь
+
+# можно коротко одной строкой
+def merge_lists_to_dict2(list_one, list_two):
+    return dict(zip(list_one, list_two))
+
+
+res3 = merge_lists_to_dict2([1, 2, 3], [True, 'abc', False])
+print(res3)
