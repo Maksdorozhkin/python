19cdc7b35cb21c23ee46e55968b4aba1866bfbbb
MD <maksdorozhkin@gmail.com>
2024-02-17 21:43:41 +0300

    Объединение двух списков с помощью функции с аргументами с ключевыми словами use dict

--- bogdan_course/functions/functions_arg_task.py
+++ bogdan_course/functions/functions_arg_task.py
@@ -1,4 +1,4 @@
-#Объединение двух списков с помощью функции с аргументами с ключевыми словами
+# Объединение двух списков с помощью функции с аргументами с ключевыми словами
 def merge_lists_to_dict(list_one, list_two):
     return dict(zip(list_one, list_two))
 
@@ -8,3 +8,14 @@ print(res)
 
 res2 = merge_lists_to_dict([1, 2, 3], list_two=[True, 'abc', False])
 print(res2)
+
+
+# функция в которой все именованные аргументы объединены в словарь
+
+def update_car_info(**car):
+    car['color'] = 'red'
+    print(car)
+    print(f"Эта машина марки {car['brand']} , стоит {car['price']} рублей, она в продаже {car['is_available']}")
+
+
+update_car_info(brand='BMW', price=3000000, is_available=True)
