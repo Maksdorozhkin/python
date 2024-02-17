9b7b4cf49c860eaaa850dad3fb9b5d9c06e7b48c
MD <maksdorozhkin@gmail.com>
2024-02-17 10:49:48 +0300

    Codewars task cockroach speed

---
+++ bogdan_course/functions/functions_arg_rep.py
@@ -0,0 +1,32 @@
+# аргументы и параметры функции
+# ошибка отсутствия аргументов
+def sum_nums(a, b):
+    c = a + b
+    return c
+
+
+print(sum_nums(2, 2))
+
+
+# объединение аргументов в кортеж
+
+
+def sum_nums1(*args):
+    print(args)
+    print(type(args))
+    print(args[0])
+    return sum(args)
+
+
+print(sum_nums1(2, 3, 4, 2))
+
+
+# использование позиционных аргументов
+
+def get_posts_info(name, posts_qty):
+    info = f"{name} wrote {posts_qty} posts"
+    return info
+
+
+info = get_posts_info('Maks', 25)
+print(info)
