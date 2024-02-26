ed64e3f40bf574f1a7c59928622508dbd37438c0
MD <maksdorozhkin@gmail.com>
2024-02-26 22:24:26 +0300

    использование statements в функциях

--- bogdan_course/statements/statements_bc_rep.py
+++ bogdan_course/statements/statements_bc_rep.py
@@ -52,3 +52,20 @@ def nums_info(a, b):
 print(nums_info(True, 10))
 print(nums_info(10, 2))
 print(nums_info(4, 15))
+
+# тоже самое только с использованием одной лишней переменной
+
+
+def nums_info2(a, b):
+    if (type(a) is not int) or (type(b) is not int):
+        info = "Один из аргументов целое число"
+    elif a >= b:
+        info = f"{a} больше или равно {b}"
+    else:
+        info = f"{a} меньше {b}"
+    return info
+
+
+print(nums_info2(True, 10))
+print(nums_info2(10, 2))
+print(nums_info2(4, 15))
