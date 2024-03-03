e6c6705e67f19ef7ab012d10cf6f939bfa47ec11
MD <maksdorozhkin@gmail.com>
2024-03-03 20:14:01 +0300

    циклы for in

---
+++ bogdan_course/cycles/cycles_for_in.py
@@ -0,0 +1,35 @@
+# цикл for in для списка для кортежа тоже самое что и для списка
+my_list = [True, 10, 'abc', {}]
+for elem in my_list:
+    print(type(elem))
+
+
+# for in for dict
+
+my_dict = {
+    'x': 10,
+    'y': True,
+    'z': 'abc'
+}
+for key in my_dict:
+    # print(key)
+    print(key, my_dict[key])  # если нужно получить значения ключей
+
+for item in my_dict.items():
+    key, value = item
+    print(key, value)
+
+for k, v in my_dict.items():
+    print(k, v)
+
+
+# цикл for in для наборов
+video_id = {123, 457, 1456, 456}
+for sets in video_id:
+    print(sets)
+
+
+# цикл for in для строк
+my_name = 'Maksim Dorozhkin'
+for symbol in my_name:
+    print(symbol.upper())
