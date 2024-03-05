a41ab5a8d629e8076e5be2db5b890f996c9d854e
MD <maksdorozhkin@gmail.com>
2024-03-05 11:00:41 +0300

    use continue

--- bogdan_course/cycles/while.py
+++ bogdan_course/cycles/while.py
@@ -1,4 +1,5 @@
 # пример цикла while
+import random
 i = 10
 while i <= 100:  # цикл работает пока i < 100
     print(i)
@@ -10,3 +11,14 @@ while True:
     answer = input("Enter yes or no: ")
     if answer == 'no':
         break
+
+# использование continue
+
+random_num = random.randint(1, 5)
+while True:
+    num = int(input("Guess the number from 1 to 5: "))
+    if num != random_num:
+        print("Try again")
+        continue
+    print("Congratulation!", random_num)
+    break
