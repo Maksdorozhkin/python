90fc79c3ddcb4a399cc2ac2be0e69923c54613a8
MD <maksdorozhkin@gmail.com>
2024-03-25 20:07:10 +0300

    введение в декораторы, разбор темы

---
+++ bogdan_course/decorator/decorator.py
@@ -0,0 +1,21 @@
+# пример создание декоратора
+def decorator_function(original_fn):
+    def wrapper_function(*args, **kwargs):  # универсальные параметры
+        # выполняется до функции my_function
+        print("Executed before function")
+        # универсальные параметры позволяют передавать в функцию любые аргументы в том числе именованные
+        result = original_fn(*args, **kwargs)
+        # выполняется после функции my_function
+        print("Executed after function")
+        return result
+    return wrapper_function
+
+
+@decorator_function  # применение декоратора
+def my_function(a, b):
+    print("This is my function!")
+    return (a, b)
+
+
+my_function(100, 50)
+# print(result)
