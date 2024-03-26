ce5162118f12d8be37fc8dee4cacbf33f7af4c7b
MD <maksdorozhkin@gmail.com>
2024-03-26 14:20:02 +0300

    что такое __main__ и __name__

--- bogdan_course/module/import_module.py
+++ bogdan_course/module/import_module.py
@@ -1,12 +1,19 @@
 # импортирование из другого модуля (файла)
-import module_practice  # as first_module чтобы переименовать модуль при импорте
+# import module_practice  # as first_module чтобы переименовать модуль при импорте
 # from module_practice import my_name, print_sum так делаем если нужно импортировать что то одно а не весь файл (модуль)
 # from module_practice import * импортирует все из файла
 # что бы переименовать переменную при импорте, from module_practice import my_name as name
 
-print(module_practice)
-print(type(module_practice))
-print(dir(module_practice))
-print(module_practice.my_name)
+# print(module_practice)
+# print(type(module_practice))
+# print(dir(module_practice))
+# print(module_practice.my_name)
 
-module_practice.print_sum(5, 7)
+# module_practice.print_sum(5, 7)
+
+def my_fn():
+    pass
+
+
+print('import_module.py', __name__)
+print('import_module.py', __name__ == '__main__')
