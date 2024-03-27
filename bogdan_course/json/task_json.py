de69482cba43800b5d50b7d4ad683560c32e9580
MD <maksdorozhkin@gmail.com>
2024-03-27 14:39:55 +0300

    решение задачи json

--- bogdan_course/json/task_json.py
+++ bogdan_course/json/task_json.py
@@ -9,6 +9,6 @@ my_dict = {
     "gear": [1, 2, 3, 4, 5, 6]
 }
 
-json_from_dict = json.dumps(my_dict)
+json_from_dict = json.dumps(my_dict, indent=1)
 print(json_from_dict)
 print(type(json_from_dict))
