a40e9a064249436752b31ac1316f7edfca89892b
MD <maksdorozhkin@gmail.com>
2024-03-27 14:35:34 +0300

    решение задачи по json

---
+++ bogdan_course/json/task_json.py
@@ -0,0 +1,14 @@
+import json
+
+
+my_dict = {
+    "bike": 'Ducati',
+    "color": 'red',
+    "vol": 1.2,
+    "like": True,
+    "gear": [1, 2, 3, 4, 5, 6]
+}
+
+json_from_dict = json.dumps(my_dict)
+print(json_from_dict)
+print(type(json_from_dict))
