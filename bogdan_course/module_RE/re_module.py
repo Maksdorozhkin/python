af06f64e866a7e7491b7b6d75e759ed397ce7e19
MD <maksdorozhkin@gmail.com>
2024-03-29 21:59:45 +0300

    используем заранее заготовленный патерн для поиска

--- bogdan_course/module_RE/re_module.py
+++ bogdan_course/module_RE/re_module.py
@@ -7,3 +7,17 @@ res = re.search('M....m$', my_string)
 res = re.search('M....m\.', my_string)
 print(res)
 print(type(res))
+
+# используем заранее заготовленный патерн для поиска
+my_pattern1 = re.compile(r'M....m\.$')
+print(my_pattern1)
+print(my_pattern1.search(my_string))
+
+my_pattern2 = re.compile(r'^My.*\.$')
+print(my_pattern2.search(my_string))
+print(my_pattern2.match(my_string))
+
+my_string2 = "My name is Maks. Maks is learning"
+
+my_pattern3 = re.compile(r'M..s')
+print(my_pattern3.findall(my_string2))
