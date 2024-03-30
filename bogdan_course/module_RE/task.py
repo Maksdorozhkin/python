45d7655647cb7029cf7b85c8f6dd0da12467fcf7
MD <maksdorozhkin@gmail.com>
2024-03-30 13:23:57 +0300

    задача написать функцию проверяющую пароль по патерну, решение на основе паттерна который создал chatgpt

--- bogdan_course/module_RE/task.py
+++ bogdan_course/module_RE/task.py
@@ -2,23 +2,23 @@ import re
 
 
 def check_password(password):
-    password_pattern1 = r"/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g"
-    # password_pattern2 = r"""^[!@#$%^&*()-_=+{}[]|\;:"<>,.?/]"""
-    if re.search(password_pattern1, password):
-        print("yes!")
-    else:
-        print("no!")
+    password_pattern = re.compile(
+        r"""^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}|:"<>?`~\-=[\]\\;',./]).{8,20}$""")
+    if len(password) < 8:
+        return (False, "Пароль должен содержать минимум 8 символов")
+    if not re.fullmatch(password_pattern, password):
+        return (False, "Пароль должен содержать заглавные и строчные цифры и спецсимволы")
+    return (True, "Password is valid")
 
 
-def get_password_from_user():
-    while True:
-        password = input(
-            "Введите пароль состоящий минимум из 8 символов содержащий заглавные и прописные буквы и специальные символы: ")
-        if len(password) < 8:
-            print("Пароль должен содержать 8 символов! ")
-            continue
-        check_password(password)
+while True:
+    password = input("Please enter password: ")
+    password_check_res = check_password(password)
+    # проверка если в в последнем return True выводит password is valid
+    if password_check_res[0]:
+        print(password_check_res[1])
         break
+    print(password_check_res[1])
 
 
-get_password_from_user()
+# решение задачи на основе паттерна который создал chatgpt)
