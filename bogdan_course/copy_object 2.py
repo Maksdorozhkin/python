a23adadf3bf68a21aec7aa08ae92f344ced24ddf
Maksim Dorozhkin <maksdorozhkin@gmail.com>
2024-04-04 20:59:40 +0300

    first commit

---
+++ bogdan_course/copy_object 2.py
@@ -0,0 +1,8 @@
+my_name = "Maks"
+print(id(my_name))
+
+my_nums = 100
+print(id(my_nums))
+
+other_nums = my_nums
+print(id(other_nums))
---
+++ bogdan_course/int 2.py
@@ -0,0 +1,34 @@
+# # friends_qty = 55
+# # print(friends_qty)
+# # print(type(friends_qty))
+#
+#
+# # any_num = int(input("Enter any number: ")) # конвертация в число строки
+# # print(any_num)
+#
+# # возведение в степень
+# base = 5
+# power = 33
+# pow_experement = (pow(base, power))
+# print(pow_experement)
+# print(type(pow_experement))
+#
+#
+# # input_str = input("Enter any number: ")
+# # input_int = int(input_str)
+#
+# # print(input_int)
+# # print(type(input_int))
+#
+long_int = 1_000_000_000_000
+print(long_int)
+any_nums = int(input("Enter any number: "))  # конвертация строки в число
+print(any_nums)
+print(type(any_nums))
+
+# возведение в степень
+base = 5
+power = 3
+
+print(pow(base, power))
+print(base)
